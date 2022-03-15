4b8022f6d2420ee396c6239ed270584afa206233
KOJIMA KAISEI <>
2022-03-16 00:43:51 +0900

    記事詳細の実装

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -14,36 +14,38 @@ RSpec.describe "Api::V1::Articles", type: :request do
 
       expect(response).to have_http_status(:ok)
       expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id", "title", "updated_at", "user"]
+      expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
   end
 
-  # describe "GET /articles/:id" do
-  #   subject { get(api_v1_articles_path(article_id)) }
+  describe "GET /articles/:id" do
+    subject { get(api_v1_articles_path(article_id)) }
 
-  #   context "指定した id の記事が存在するとき" do
-  #     let(:article) { create(:article) }
-  #     let(:article_id) { article.id }
+    context "指定した id の記事が存在するとき" do
+      let(:article) { create(:article) }
+      let(:article_id) { article.id }
 
-  #     it "指定した id の記事を取得できる" do
-  #       subject
-  #       res = JSON.parse(response.body)
-  #       expect(response).to have_http_status(200)
-  #       expect(res[0]["id"]).to eq article.id
-  #       expect(res[0]["title"]).to eq article.title
-  #       expect(res[0]["body"]).to eq article.body
-  #       expect(res[0]["updated_at"]).to be_present
-  #       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
-  #     end
-  #   end
+      it "指定した id の記事を取得できる" do
+        subject
+        res = JSON.parse(response.body)
+        expect(response).to have_http_status(:ok)
+        expect(res[0]["id"]).to eq article.id
+        expect(res[0]["title"]).to eq article.title
+        expect(res[0]["body"]).to eq article.body
+        expect(res[0]["updated_at"]).to be_present
+        expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+      end
+    end
 
-  #   # context "指定した id の記事が存在しないとき" do
-  #   #   let(:article_id) { 10000 }
+    context "指定した id の記事が存在しないとき" do
+      let(:article_id) { 10000 }
 
-  #   #   fit "記事が見つからない" do
-  #   #     expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
-  #   #   end
-  #   # end
-  # end
+      fit "記事が見つからない" do
+binding.pry
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+binding.pry
+      end
+    end
+  end
 end
