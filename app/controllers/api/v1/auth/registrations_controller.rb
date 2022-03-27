5eb96ab0ff74c76c24048253a81f29cc7e65284c
KOJIMA KAISEI <>
2022-03-28 07:37:44 +0900

    rubocop による修正

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-
   private
 
-    def sign_up_params #新規登録用のメソッド
+    def sign_up_params # 新規登録用のメソッド
       params.permit(:name, :email, :password, :password_confirmation)
     end
 
-    def account_update_params #更新用のメソッド
+    def account_update_params # 更新用のメソッド
       params.permit(:name, :email)
     end
-
 end
