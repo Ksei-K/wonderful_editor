d9921892bae66b28c55deff87d0b9a9afed4ccde
KOJIMA KAISEI <>
2022-04-10 22:32:24 +0900

    既存のAPIの修正

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -4,37 +4,57 @@ RSpec.describe "Api::V1::Articles", type: :request do
   describe "GET /articles" do
     subject { get(api_v1_articles_path) }
 
-    let!(:article1) { create(:article) }
-    let!(:article2) { create(:article) }
-    let!(:article3) { create(:article) }
+    let!(:article1) { create(:article, :published, updated_at: 1.days.ago) }
+    let!(:article2) { create(:article, :published, updated_at: 2.days.ago) }
+    let!(:article3) { create(:article, :published) }
 
-    it "記事の一覧が取得できる" do
+    before { create(:article, :draft) }
+
+    it "公開している記事の一覧が取得できる" do
       subject
       res = JSON.parse(response.body)
 
       expect(response).to have_http_status(:ok)
       expect(res.length).to eq 3
-      expect(res[0].keys).to eq ["id", "title", "body", "updated_at", "user"]
+      expect(res[0].keys).to eq ["id", "title", "body", "status", "updated_at", "user"]
       expect(res[0]["user"].keys).to eq ["id", "name", "email"]
     end
   end
 
   describe "GET /articles/:id" do
-    subject { get(api_v1_articles_path(article_id)) }
+    subject { get(api_v1_article_path(article_id)) }
 
-    context "指定した id の記事が存在するとき" do
-      let(:article) { create(:article) }
+    context "指定した id の記事が公開されているとき" do
       let(:article_id) { article.id }
+      let(:article) { create(:article, :published) }
 
       it "指定した id の記事を取得できる" do
         subject
         res = JSON.parse(response.body)
+
         expect(response).to have_http_status(:ok)
-        expect(res[0]["id"]).to eq article.id
-        expect(res[0]["title"]).to eq article.title
-        expect(res[0]["body"]).to eq article.body
-        expect(res[0]["updated_at"]).to be_present
-        expect(res[0]["user"].keys).to eq ["id", "name", "email"]
+        expect(res["id"]).to eq article.id
+        expect(res["title"]).to eq article.title
+        expect(res["body"]).to eq article.body
+        expect(res["updated_at"]).to be_present
+        expect(res["user"].keys).to eq ["id", "name", "email"]
+      end
+    end
+
+    context "指定した id の記事が下書きのとき" do
+      let(:article_id) { article.id }
+      let(:article) { create(:article, :draft) }
+
+      it "記事が見つからない" do
+        expect { subject }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "指定した id の記事が存在しない場合" do
+      let(:article_id) { 10000 }
+
+      it "記事が見つからない" do
+        expect { subject }.to raise_error ActiveRecord::RecordNotFound
       end
     end
   end
